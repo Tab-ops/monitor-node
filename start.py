41f45cd7add97c6ee6eceeae43d61bcaa2078351
taobo <313835125@qq.com>
2022-05-15 21:06:34 +0800

    monitor

--- start.py
+++ start.py
@@ -1,3 +1,18 @@
+from requests import head
+
+
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@File    :   start.py
+@Time    :   2022/05/15 21:05:58
+@Author  :   taobo
+@Version :   1.0
+@Contact :   taobo@btcex.com
+@License :   (C)Copyright 2019-2022
+@Desc    :   None
+"""
+
 import sys,os
 #将环境目录增加到path目录
 path=os.path.dirname(__file__)
